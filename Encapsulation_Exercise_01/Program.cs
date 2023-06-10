9d71cf887953873db149d7576874733b56fc821a
johndward01 <johndward01@gmail.com>
2023-06-10 01:46:49 -0500

    update

--- Encapsulation_Exercise_01/Program.cs
+++ Encapsulation_Exercise_01/Program.cs
@@ -3,19 +3,17 @@
 // Use methods to encapsulate these fields with validations.
 
 // TODO: Use a property to encapsulate the username field
-// TODO: In the getter, if the username is null throw new ArgumentNullException("The username must not be null");
-// TODO: In the getter, if the username is empty throw new ArgumentException("The username must not be empty");
+// In the setter, if the username is null throw new ArgumentNullException("The username must not be null");
+// In the setter, else if the username is empty throw new ArgumentException("The username must not be empty");
+// In the setter, else set the username = value;
 
 // TODO: Use a method to encapsulate the email field
 // TODO: The method ValidateEmail(string email) will return true if
-// the email contains an @ symbol 
-// the email contains a . 
-// the emails are only from gmail, yahoo, or outlook
+// the email contains an @ symbol (this is a simplified example, consider researching email validation regex c# for a more realistic implementation)
 
 // TODO: Use either a method or a property to validate the password field
 // the password should be at least 10 characters long
 // the password should contain at least 2 special symbols [!@#$%^&*()]
 // the password should contain at least 2 uppercase letters
-// the password should contain at least 2 lowercase letters
 
 
--- Encapsulation_Exercise_01/User.cs
+++ Encapsulation_Exercise_01/User.cs
@@ -6,5 +6,25 @@ internal class User
     private string email;
     private string password;
 
+    public string UserName
+    {
+        get => username;
+        set
+        {
+            if (username == null)
+            {
+                throw new ArgumentNullException("The username must not be null");
+            }
+            else if (username.Length == 0)
+            {
+                throw new ArgumentException("The username must not be empty");
+            }
+            else
+            {
+                username = value;
+            }
+        }
+    }
+
 
 }
