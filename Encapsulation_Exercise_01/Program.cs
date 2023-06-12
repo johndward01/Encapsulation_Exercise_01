a55bb3451f65bee8c751334c81bf689dc6b6d480
johndward01 <johndward01@gmail.com>
2023-06-12 03:32:58 -0500

    finished

--- Encapsulation_Exercise_01/Program.cs
+++ Encapsulation_Exercise_01/Program.cs
@@ -1,25 +1,27 @@
 ﻿using Encapsulation_Exercise_01;
 
-// TODO: Create a User class with 3 private fields for username, password, and email
+// TODO: Create a User class with 3 private fields for _username, _password, and _email
 // TODO: Initialize the 3 fields with an empty string ""
 
 
-// TODO: Use a property to encapsulate the username field
-// In the setter, if the username is null throw new ArgumentNullException("The username must not be null");
-// In the setter, else if the username is empty throw new ArgumentException("The username must not be empty");
-// In the setter, else set the username = value;
+// TODO: Use a property to encapsulate the _username field
+// In the setter, if the value is null throw new ArgumentNullException("The username must not be null");
+// In the setter, else if the value is empty throw new ArgumentException("The username must not be empty");
+// In the setter, else set the _username = value;
 
 
-// TODO: Use a property to encapsulate the email field
-// In the setter, if the the email doesn't contain an @ symbol throw new ArgumentException("The email must contain an @ symbol");
+// TODO: Use a property to encapsulate the _email field
+// In the setter, if the the _email doesn't contain an @ symbol throw new ArgumentException("The email must contain an @ symbol");
+// In the setter, else set the _email = value;
 
 
-// TODO: Create 2 methods to encapsulate the password field
+// TODO: Create 2 methods to encapsulate the _password field
 // 1 called GetPassword() which returns the value stored in the _password field
-// 1 called SetValidPassword() which validates the password before setting the field
+// 1 called SetValidPassword() which validates the _password field before setting it
 // the password should be at least 10 characters long
 // the password should contain at least 2 special symbols [!@#$%^&*()]
 // the password should contain at least 2 uppercase letters
+
 
 // TODO: Create a new User and give the 3 fields valid values
 var user1 = new User();
