d3ba7a47141ad284369e3dea06019fe6306c40b0
johndward01 <johndward01@gmail.com>
2023-06-12 02:23:33 -0500

    update

--- Encapsulation_Exercise_01/Program.cs
+++ Encapsulation_Exercise_01/Program.cs
@@ -2,16 +2,20 @@
 // TODO: Create a User class with private fields for username, password, and email.
 // Use methods to encapsulate these fields with validations.
 
+
 // TODO: Use a property to encapsulate the username field
 // In the setter, if the username is null throw new ArgumentNullException("The username must not be null");
 // In the setter, else if the username is empty throw new ArgumentException("The username must not be empty");
 // In the setter, else set the username = value;
 
-// TODO: Use a method to encapsulate the email field
-// TODO: The method ValidateEmail(string email) will return true if
-// the email contains an @ symbol (this is a simplified example, consider researching email validation regex c# for a more realistic implementation)
 
-// TODO: Use either a method or a property to validate the password field
+// TODO: Use a property to encapsulate the email field
+// In the setter, if the the email doesn't contain an @ symbol throw new ArgumentException("The email must contain an @ symbol");
+
+
+// TODO: Create 2 methods to encapsulate the password field
+// 1 called GetPassword() which returns the value stored in the _password field
+// 1 called SetValidPassword() which validates the password before setting the field
 // the password should be at least 10 characters long
 // the password should contain at least 2 special symbols [!@#$%^&*()]
 // the password should contain at least 2 uppercase letters
--- Encapsulation_Exercise_01/User.cs
+++ Encapsulation_Exercise_01/User.cs
@@ -2,29 +2,107 @@
 
 internal class User
 {
-    private string username;
-    private string email;
-    private string password;
+    private string _username;
+    private string _email;
+    private string _password;
 
     public string UserName
     {
-        get => username;
+        get => _username;
         set
         {
-            if (username == null)
+            if (_username == null)
             {
                 throw new ArgumentNullException("The username must not be null");
             }
-            else if (username.Length == 0)
+            else if (_username.Length == 0)
             {
                 throw new ArgumentException("The username must not be empty");
             }
             else
             {
-                username = value;
+                _username = value;
+            }
+        }
+    }
+    public string Email 
+    {
+        get => _email;
+        set
+        {
+            if (!_email.Contains('@'))
+            {
+                throw new ArgumentException("The email must contain an @ symbol");
+            }
+            else
+            {
+                _email = value;
             }
         }
     }
 
+    public string GetPassword() => _password;
+    public void SetValidPassword(string password)
+    {
+        bool lengthCheck = LengthCheck(password);
+        bool symbolCheck = SymbolCheck(password);
+        bool upperCaseCheck = LengthCheck(password);
+
+        if (lengthCheck && symbolCheck && upperCaseCheck)
+        {
+            _password = password;
+        }
+    }
+
+    public bool LengthCheck(string password)
+    {
+        if (password.Length > 10)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool SymbolCheck(string password)
+    {
+        var symbols = "!@#$%^&*()";
+        var symbolCount = 0;
+
+        foreach (var letter in password)
+        {
+            foreach (var symbol in symbols)
+            {
+                if (letter == symbol)
+                {
+                    symbolCount++;
+                }
+            }
+        }
+
+        if (symbolCount >= 2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool UpperCaseCheck(string password)
+    {
+        var upperCaseCount = 0;
+
+        foreach (var letter in password)
+        {
+            if (letter == char.ToUpper(letter))
+            {
+                upperCaseCount++;
+            }
+        }
+
+        if (upperCaseCount >= 2)
+        {
+            return true;
+        }
+        return false;
+    }
 
 }
